d041378f1bd94ab1725bb069cb8833abddc5daad
dotdit99 <dotdit99@icloud.com>
2026-04-15 11:45:44 +0900

    feat: core - App 및 index 파일 설정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import PopularPage from "./pages/PopularPage";
+import FreeboardPage from "./pages/FreeboardPage";
+import InfoPage from "./pages/InfoPage";
+import QnaPage from "./pages/QnaPage";
+import EasterEgg from "./components/EasterEgg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <EasterEgg />
+      <Routes>
+        {/* 메인 페이지 - 모든 게시판 한 곳에 표시 */}
+        <Route path="/" element={<MainPage />} />
+
+        {/* 각 게시판 페이지 */}
+        <Route path="/popular" element={<PopularPage />} />
+        <Route path="/freeboard" element={<FreeboardPage />} />
+        <Route path="/info" element={<InfoPage />} />
+        <Route path="/qna" element={<QnaPage />} />
+      </Routes>
+    </Router>
   );
 }
 
